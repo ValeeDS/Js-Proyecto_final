87531b090449e31a7b32af476fd49990bf967680
Valeria Del Savio <valeria.delsavio@hotmail.com>
2023-03-18 20:31:50 -0300

    close btn

--- entrega.js
+++ entrega.js
@@ -251,10 +251,12 @@ class Factura {
         `<div class="modal-dialog relative h-full w-fit md:h-auto">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow max-h-[calc(100vh-2rem)] overflow-y-scroll">
-                <button type="button" class="btn-close absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="crypto-modal">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                    <span class="sr-only">Close modal</span>
-                </button>
+                <div class="z-30 absolute top-3 right-2.5">
+                    <button type="button" class="btn-close text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="modalFact">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
                 <!-- Modal header -->
                 <div class="px-6 py-4 border-b rounded-t sticky top-0 bg-white z-20  shadow-sm"><h4 class="text-base font-semibold text-gray-900 lg:text-xl">Factura: ${this.puntoVenta.toString().padStart(4,'0')}-${this.num.toString().padStart(8,'0')}</h4></div>
                 <!-- Modal body -->
